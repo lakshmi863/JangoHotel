6baee72963272394021bd9271610d4ca867927cd
lakshmi863 <sathya.lakshmipathi@gmail.com>
2025-07-07 07:07:31 +0530

    changesneed

--- LoginApp/settings.py
+++ LoginApp/settings.py
@@ -130,6 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/home/'  
 LOGOUT_REDIRECT_URL = "App: login"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
+
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
